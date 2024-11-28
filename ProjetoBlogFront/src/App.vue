dbc95623e760221286f3b7f400beb037f0093a20
Marcelo de Souza <marcelodesouzasj@gmail.com>
2024-11-28 14:24:39 -0300

    PrimerioCommit

---
+++ ProjetoBlogFront/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <router-link to="/login">Login</router-link>
+  <router-link to="/about">About</router-link>
+  <router-link to="/home">Home</router-link>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
